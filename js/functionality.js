d60f64eb80f6141ca6d5462ec96900cfb3d50366
Souvik <39402251+souvikxyz@users.noreply.github.com>
2022-03-04 05:33:01 +0530

    Update functionality.js

--- js/functionality.js
+++ js/functionality.js
@@ -4,9 +4,7 @@ var wdt=window.screen.width;
 var hgt_scaled=hgt*scale;
 var wdt_scaled=wdt*scale;
 var r=wdt_scaled/hgt_scaled;
-var siteWidth = 1280;
-var scale1 = screen.width /siteWidth;
-console.log(scale1);
+
 document.getElementById("width_value").setAttribute("value",wdt_scaled);
 document.getElementById("length_value").setAttribute("value",hgt_scaled);
 document.getElementById("main").innerHTML = document.getElementById('op1').innerHTML;
